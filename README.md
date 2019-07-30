e724087ea89da5ca2db097455f947cec0fd03121
Xy <kasuo.xy@gmail.com>
2019-07-30 14:27:16 +0800

    运行后访问127..0.0.1:8080/111/222/index.html查看效果

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # rust
-Rust_lang study
+Rust_lang学习
 ---
-rust语言学习
+actix-web简单应用
+使用了rustup、cargo、gcc.exe
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 extern crate actix_web;
-use actix_web::{serve, App, HttpRequest};
 
-fn index(_req: &HttpRequest) -> &'static str {
-	"Hello world!"
+use actix_web::{server, App, Path, Responder};
+
+fn index(info: Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
 }
 
 fn main() {
-	server::new(|| App::new().resource("/", |r| r.f(index)))
-		.bind("127.0.0.1:8088")
-		.unwarp()
-		.run();
+    server::new(|| {
+        App::new().resource("/{name}/{id}/index.html", |r| r.with(index))
+    }).bind("127.0.0.1:8080")
+        .unwrap()
+        .run();
 }
